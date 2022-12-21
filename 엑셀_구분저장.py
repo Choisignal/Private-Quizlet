b984b0df61968aa253577c673dc09872f6eb00d4
Choisignal <98930895+Choisignal@users.noreply.github.com>
2022-12-21 18:38:28 +0900

    Update 엑셀_구분저장.py
    단답형, 객관식 구분 기능 추가

--- 엑셀_구분저장.py
+++ 엑셀_구분저장.py
@@ -31,5 +31,29 @@ def 엑셀파일구분하기(data_direct, filename):
 
 if __name__ == "__main__":
     data_direct = "./학습자료/단답형/"
-    filename = "국어_"
-    엑셀파일구분하기(data_direct, filename)
+    filename = "국어_복습"
+    단답형_목록 = ["발음","고쳐쓰기"]
+    객관식_목록 = []
+    #엑셀파일구분하기(data_direct, filename)
+    data = pd.read_excel(f"{data_direct}{filename}.xlsx")
+    구분목록 = list(set(data["구분"]))
+    for 구분 in 구분목록:
+        if 구분 not in 단답형_목록:
+            객관식_목록 += [구분]
+    # 단답형 추출
+    if len(단답형_목록) > 0:
+        data_단답형1 = data[data["구분"] == 단답형_목록[0]]
+        if len(단답형_목록) > 1:
+            for 단답형 in 단답형_목록[1:]:
+                data_단답형2 = data[data["구분"] == 단답형]
+                data_단답형1 = pd.concat([data_단답형1,data_단답형2],ignore_index=True)
+        data_단답형1.to_excel(f"{data_direct}{filename}_단답형.xlsx")
+
+    if len(객관식_목록) > 0:
+        # 객관식 추출
+        data_객관식1 = data[data["구분"] == 객관식_목록[0]]
+        if len(객관식_목록) > 1:
+            for 객관식 in 객관식_목록[1:]:
+                data_객관식2 = data[data["구분"] == 객관식]
+                data_객관식1 = pd.concat([data_객관식1,data_객관식2],ignore_index=True)
+        data_객관식1.to_excel(f"{data_direct}{filename}_객관식.xlsx")
