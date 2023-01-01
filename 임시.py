1c57e4e21f1d7d3d4765fb30cb0db0df7af090bd
Choisignal <98930895+Choisignal@users.noreply.github.com>
2023-01-01 10:46:31 +0900

    데이터 커밋
    데이터 커밋

--- 임시.py
+++ 임시.py
@@ -1,40 +1,17 @@
-from pathlib import Path
-from pandas import read_excel, DataFrame
-from datetime import datetime
+# python script showing battery details
+import psutil
 
+# function returning time in hh:mm:ss
+def convertTime(seconds):
+	minutes, seconds = divmod(seconds, 60)
+	hours, minutes = divmod(minutes, 60)
+	return "%d:%02d" % (hours, minutes)
 
-def 학습시간(모드, study_hour=0):
-    now = datetime.now()
-    day = now.strftime("%Y%m%d")
-    my_file = Path("학습시간.xlsx")
-    if my_file.is_file():
-        df = read_excel("학습시간.xlsx")
-    else:
-        df = DataFrame({'날짜': [day], '학습시간': [0]})
-        df.to_excel("학습시간.xlsx", index=False)
+# returns a tuple
+battery = psutil.sensors_battery()
 
-    if 모드 == '읽기':
-        날짜목록 = list(df['날짜'])
-        if int(day) in 날짜목록:
-            study_hour = int(df[df['날짜'] == int(day)]['학습시간'])
-        else:
-            df['날짜'][-1] = day
-            df['학습시간'][-1] = 0
-            study_hour = 0
-    elif 모드 == '쓰기':
-        날짜목록 = list(df['날짜'])
-        last_study_time = int(df[df['날짜'] == int(day)]['학습시간'])
-        if int(day) in 날짜목록:
-            df.loc[df['날짜'] == int(day), '학습시간'] = last_study_time + study_hour
-        else:
-            df['날짜'][-1] = day
-            df['학습시간'][-1] = study_hour
-        df.to_excel("학습시간.xlsx", index=False)
-        study_hour = int(df[df['날짜'] == int(day)]['학습시간'])
-    return study_hour
+print("Battery percentage : ", battery.percent)
+print("Power plugged in : ", battery.power_plugged)
 
-
-study_hour = 학습시간(모드="읽기", study_hour=0)
-print(type(study_hour), study_hour)
-study_hour = 학습시간(모드="쓰기", study_hour=1)
-print(type(study_hour), study_hour)
+# converting seconds to hh:mm:ss
+print("Battery left : ", convertTime(battery.secsleft))
