099b0c5334295731406045e2370d0845d84be580
Choisignal <98930895+Choisignal@users.noreply.github.com>
2023-01-14 10:09:03 +0900

    Update 엑셀 파일 편집.py
    함수 정리

--- 엑셀 파일 편집.py
+++ 엑셀 파일 편집.py
@@ -1,38 +1,33 @@
 import pandas as pd
 
-file = "./학습자료/단답형/한국사 종합.xlsx"
-data = pd.read_excel(file)
-data.to_excel(file.replace(".xlsx","_백업.xlsx"))
-ask_list = list(data["질문"])
-ans_list = list(data["대답"])
-cat_list = list(data["구분"])
+file = "./학습자료/단답형/국어_복습_객관식.xlsx"
+def 수정(단어,이전,이후):
+    if 이전 in 단어:
+        print("수정 전 : ",단어)
+        단어 = 단어.replace(이전,이후)
+        print("수정 후 : ",단어,"\n")
+    
+def 파일수정(file = "./학습자료/단답형/국어_복습_객관식.xlsx"):
+    data = pd.read_excel(file)
+    data.to_excel(file.replace(".xlsx","_백업.xlsx"))
+    질문_list = list(data["질문"])
+    대답_list = list(data["대답"])
+    구분_list = list(data["구분"])
 
-for i in range(len(ask_list)):
-    ask = ask_list[i]
-    ans = ans_list[i]
-    cat = cat_list[i]
-    ask = str(ask).strip()
-    ans = str(ans).strip()
-    cat = str(cat).strip()
-    '''
-    length = len("③ 역할 : 농민의 불만과 개혁 요구를 반영하기 위해 정치·경제·사회 등")
-    if ":" in ask:
-        print(ask)
-        ask_list2 = ask.split("\n")
-        ask = ""
-        for asks in ask_list2:
-            if len(asks) >= length:
-                asks = asks.replace(":",":\n")
-            ans += asks + "\n"
-        ask = ask[:-1]
-    '''
-    ask = ask.replace(".",",")
-    if ask[-1] == ",":
-        ask = ask[:-1]
-    data.loc[i,"질문"] = ask
-    data.loc[i,"대답"] = ans
-    data.loc[i,"구분"] = cat
+    for i in range(len(질문_list)):
+        질문 = 질문_list[i]
+        대답 = 대답_list[i]
+        구분 = 구분_list[i]
+        질문 = str(질문).strip()
+        대답 = str(대답).strip()
+        구분 = str(구분).strip()
+        
+        수정(대답,"깍두기//깍두기","깍두기//깍뚜기")
+        
+        data.loc[i,"질문"] = 질문
+        data.loc[i,"대답"] = 대답
+        data.loc[i,"구분"] = 구분
+    data.to_excel(file)
 
-    if ":" in ask:
-        print(ask)
-data.to_excel(file)
+파일수정(file = "./학습자료/단답형/국어_복습_객관식.xlsx")
+파일수정(file = "./학습자료/단답형/국어_복습.xlsx")
