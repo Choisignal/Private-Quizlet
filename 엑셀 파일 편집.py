5b535434d1e3c69f59e6fd71007290343010cc22
Choisignal <98930895+Choisignal@users.noreply.github.com>
2023-02-14 16:19:01 +0900

    데이터 커밋
    데이터 커밋

--- 엑셀 파일 편집.py
+++ 엑셀 파일 편집.py
@@ -14,20 +14,8 @@ def 확인(질문,대답,구분,검색):
     elif 검색 in 대답:
         print(f"{질문.replace(검색,f'[{검색}]'):20} : {대답.replace(검색,f'[{검색}]'):20} : {구분:20}")
 
-def 파일수정(file = "./학습자료/단답형/국어_복습_객관식.xlsx"):
-    data = pd.read_excel(file)
-    data.to_excel(file.replace(".xlsx","_백업.xlsx"),index=False)
-    질문_list = list(data["질문"])
-    대답_list = list(data["대답"])
-    구분_list = list(data["구분"])
-
-    for i in range(len(질문_list)):
-        질문 = 질문_list[i]
-        대답 = 대답_list[i]
-        구분 = 구분_list[i]
-        질문 = str(질문).strip()
-        대답 = str(대답).strip()
-        구분 = str(구분).strip()
+### 국어 한자
+'''
         if 구분 == "국한":
             질문 = re.findall('\(([^)]+)', 대답)[0]
             대답 = re.sub('\([^)]+\)', '', 대답)
@@ -46,6 +34,26 @@ def 파일수정(file = "./학습자료/단답형/국어_복습_객관식.xlsx")
             if 대답[0] == 대답[1]:
                 구분 = "한자 2글자 동음"
                 print(f"{질문}\n{대답}\n{구분}\n\n")
+'''
+def 파일수정(file = "./학습자료/단답형/국어_복습_객관식.xlsx"):
+    data = pd.read_excel(file)
+    data.to_excel(file.replace(".xlsx","_백업.xlsx"),index=False)
+    질문_list = list(data["질문"])
+    대답_list = list(data["대답"])
+    구분_list = list(data["구분"])
+
+    for i in range(len(질문_list)):
+        질문 = 질문_list[i]
+        대답 = 대답_list[i]
+        구분 = 구분_list[i]
+        질문 = str(질문).strip()
+        대답 = str(대답).strip()
+        구분 = str(구분).strip()
+
+        if len(대답.split(","))>=2:
+            구분 = 구분 + f' {len(대답.split(","))}명'
+            print(f"{질문}\n{대답}\n{구분}\n\n")
+
         data.loc[i,"질문"] = 질문
         data.loc[i,"대답"] = 대답
         data.loc[i,"구분"] = 구분
@@ -53,4 +61,4 @@ def 파일수정(file = "./학습자료/단답형/국어_복습_객관식.xlsx")
 
 #파일수정(file = "./학습자료/단답형/국어_복습_객관식.xlsx")
 #파일수정(file = "./학습자료/단답형/영어_복습.xlsx")
-파일수정(file = "./학습자료/단답형/국어 파이널.xlsx")
+파일수정(file = "./학습자료/단답형/한국사 인물.xlsx")
